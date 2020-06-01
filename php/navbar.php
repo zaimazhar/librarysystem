2d2f863ca97d414788be12ed4d881159d2c134fa
Zaim-PC <zaim.azhar97@gmail.com>
2020-06-01 09:36:23 +0800

    Second commit

---
+++ php/navbar.php
@@ -0,0 +1,18 @@
+<nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">library</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index.php" style="vertical-align: middle;"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 1px;">home</span> Home</a></li>
+                <li><a href="library.php" style="vertical-align: middle;"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 1px;">search</span> Browse</a></li>
+                <li><a href="login.php" style="vertical-align: middle;"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 1px;">perm_identity</span> Login</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <ul class="sidenav" id="mobile-demo">
+        <br>
+        <li><a href="index.php" style="vertical-align: middle;"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 5px;">home</span> Home</a></li>
+        <li><a href="library.php" style="vertical-align: middle;"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 5px;">search</span> Browse</a></li>
+        <li><a href="login.php"><span class="material-icons" style="vertical-align: text-bottom; margin-right: 5px;">perm_identity</span> Login</a></li>
+    </ul>
